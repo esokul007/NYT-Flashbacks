b397f52e55aea532f2ede469bfb4e190c03c4d3f
Jacob Lukose <esokul007@gmail.com>
2025-03-22 23:16:56 +0000

    Logic not working for increments

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import sqlite3, os, random, sys
-import db_handling, api_handling
+from NYT_Flashbacks.app import db_handling, api_handling
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
@@ -10,7 +10,7 @@ def home():
     if 'index' not in session:
         session['index'] = 0
         session['curr_event'] = api_handling.getEvent(random.randint(1, 12), random.randint(0, 200))
-        session['next_event'] = api_handling.getEvent(random.randint(1, 12), random.randint(0, 200))
+        session['next_event'] = api_handling.getEvent(random.randint(1, 12), random.randint(0, 200), int(session['curr_event'][0]))
     lost = False
     index = session['index']
 
@@ -23,7 +23,7 @@ def home():
             lost= int(session['next_event'][0]) < int(session['curr_event'][0])
         session['index'] += 1
         session['curr_event']=session['next_event']
-        session['next_event'] = api_handling.getEvent(random.randint(1, 12),  random.randint(0, 200))
+        session['next_event'] = api_handling.getEvent(random.randint(1, 12), random.randint(0, 200), int(session['curr_event'][0]))
     if (lost):
         session['index'] = 0
     return render_template("home.html", curr_event=session['curr_event'], next_event=session['next_event'], i=session['index'])
@@ -38,5 +38,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.debug = True
     app.run()
--- app/api_handling.py
+++ app/api_handling.py
@@ -1,13 +1,19 @@
-import requests, time
+import requests, time, random
 
-def getEvent(month, offset):
+def getEvent(month, offset, year=1900):
     api_url = 'https://api.api-ninjas.com/v1/historicalevents'
-    with open('keys/history.txt', 'r', encoding='utf-16') as f:
+    with open('NYT_Flashbacks/app/keys/history.txt', 'r', encoding='utf-16') as f:
         api_key = f.read().strip()
     final_list = []
+    if year + 10 < 2020:
+        max_year = year+10
+    else:
+        max_year = 2020
+    min_year = year -10
     params={
         'month' : month,
-        'offset' : offset
+        'offset' : offset,
+        'year' : random.randint(min_year, max_year)
     }
     # Replace 'YOUR_API_KEY' with your actual API key
     response = requests.get(api_url, headers={'X-Api-Key': api_key}, params=params)
